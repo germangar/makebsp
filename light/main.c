91950e379aea7c537175340d0e99ac2fc316b2b9
germangar <germangar@gmail.com>
2026-03-20 09:43:04 +0100

    More cleanup

--- light/main.c
+++ light/main.c
@@ -26,7 +26,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 int LightMain(int argc, char **argv);
 int VLightMain(int argc, char **argv);
-int VSoundMain(int argc, char **argv);
 
 int main(int argc, char **argv) {
   _printf("Q3Light v1.0 (c) 1999 Id Software Inc.\n");
@@ -34,8 +33,7 @@ int main(int argc, char **argv) {
   if (argc < 2) {
     Error("usage: light [options] bspfile\n"
           "Standard lighting: light -light [options] bspfile\n"
-          "Vertex lighting:   light -vlight [options] bspfile\n"
-          "Sound lighting:    light -vsound [options] bspfile\n");
+          "Vertex lighting:   light -vlight [options] bspfile\n");
   }
 
   if (!strcmp(argv[1], "-light")) {
@@ -43,9 +41,6 @@ int main(int argc, char **argv) {
   }
   if (!strcmp(argv[1], "-vlight")) {
     return VLightMain(argc - 1, argv + 1);
-  }
-  if (!strcmp(argv[1], "-vsound")) {
-    return VSoundMain(argc - 1, argv + 1);
   }
 
   // Default to -light if no recognizable flag is provided
