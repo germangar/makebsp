4a62273ab1fba815fe8f1c5556c63c810e8af8cd
germangar <germangar@gmail.com>
2026-03-22 15:42:52 +0100

    Upload the radeonrays library

---
+++ libs/spdlog/build/CMakeFiles/spdlog.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/CODE/antigravity/makebsp/libs/spdlog/source/spdlog-1.x/src/async.cpp" "CMakeFiles/spdlog.dir/src/async.cpp.obj" "gcc" "CMakeFiles/spdlog.dir/src/async.cpp.obj.d"
+  "E:/CODE/antigravity/makebsp/libs/spdlog/source/spdlog-1.x/src/bundled_fmtlib_format.cpp" "CMakeFiles/spdlog.dir/src/bundled_fmtlib_format.cpp.obj" "gcc" "CMakeFiles/spdlog.dir/src/bundled_fmtlib_format.cpp.obj.d"
+  "E:/CODE/antigravity/makebsp/libs/spdlog/source/spdlog-1.x/src/cfg.cpp" "CMakeFiles/spdlog.dir/src/cfg.cpp.obj" "gcc" "CMakeFiles/spdlog.dir/src/cfg.cpp.obj.d"
+  "E:/CODE/antigravity/makebsp/libs/spdlog/source/spdlog-1.x/src/color_sinks.cpp" "CMakeFiles/spdlog.dir/src/color_sinks.cpp.obj" "gcc" "CMakeFiles/spdlog.dir/src/color_sinks.cpp.obj.d"
+  "E:/CODE/antigravity/makebsp/libs/spdlog/source/spdlog-1.x/src/file_sinks.cpp" "CMakeFiles/spdlog.dir/src/file_sinks.cpp.obj" "gcc" "CMakeFiles/spdlog.dir/src/file_sinks.cpp.obj.d"
+  "E:/CODE/antigravity/makebsp/libs/spdlog/source/spdlog-1.x/src/spdlog.cpp" "CMakeFiles/spdlog.dir/src/spdlog.cpp.obj" "gcc" "CMakeFiles/spdlog.dir/src/spdlog.cpp.obj.d"
+  "E:/CODE/antigravity/makebsp/libs/spdlog/source/spdlog-1.x/src/stdout_sinks.cpp" "CMakeFiles/spdlog.dir/src/stdout_sinks.cpp.obj" "gcc" "CMakeFiles/spdlog.dir/src/stdout_sinks.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
