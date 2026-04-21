656a21eb34d8b919d9ac0943118badbdc487fdce
germangar <germangar@gmail.com>
2026-04-21 15:07:58 +0200

    move study/report files into their own directory

--- testmapbuild - Embree.bat
+++ testmapbuild - Embree.bat
@@ -1,4 +1,4 @@
 q3map.exe -samplesize 4 -forceuvgen -snapuvs maps/modeltest.map
-light.exe -smooth 0 -radiosity 4 -rad_bounce_scale 1.0 -rad_color_ratio 1.0 -rad_anglematch 60 maps/modeltest.bsp
+light.exe -smooth 1 -radiosity 4 -rad_bounce_scale 1.0 -rad_color_ratio 1.0 maps/modeltest.bsp
 copy /Y maps\modeltest.bsp "C:\Users\German\Documents\My Games\Warfork 2.1\basewf\maps"
 pause
