e7abad27a1547047828fb3a25a0de81ac7c2e0c8
germangar <germangar@gmail.com>
2026-05-18 10:51:50 +0200

    delete a dobument file.

--- testmapbuild - Embree - deluxe.bat
+++ testmapbuild - Embree - deluxe.bat
@@ -1,5 +1,5 @@
-q3map.exe -samplesize 2 maps/modeltest.map
+q3map.exe -samplesize 4 maps/modeltest.map
 q3map.exe -vis maps/modeltest.map
-light.exe -radiosity 4 -smooth 4 -smoothradius 0.35 -rad_intensity 0.5 -deluxe_minangle 35 maps/modeltest.bsp
+light.exe -radiosity 4 -smooth 4 -smoothradius 0.35 -rad_intensity 0.75 -deluxe_minangle 30 -rad_interval 16 maps/modeltest.bsp
 copy /Y maps\modeltest.bsp "C:\Users\German\Documents\My Games\Warfork 2.1\basewf\maps"
 pause
