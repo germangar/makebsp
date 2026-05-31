84285de272c53a4fa4709f3ca52a7dc81cd173c4
germangar <germangar@gmail.com>
2026-05-31 12:26:15 +0200

    increase MAX_PORTALS

--- q3map/vis.h
+++ q3map/vis.h
@@ -28,7 +28,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "../common/cmdlib.h"
 #include "../common/mathlib.h"
 
-#define MAX_PORTALS 32768
+#define MAX_PORTALS 65536
 
 #define PORTALFILE "PRT1"
 
