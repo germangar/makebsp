24d26452727b224314b626ea502d21745e0ec071
germangar <germangar@gmail.com>
2026-08-10 04:58:37 +0200

    change the default grid size to 64 64 64

--- shared/globals.c
+++ shared/globals.c
@@ -58,7 +58,7 @@ game_t gameTemplates[MAX_GAMES] = {
 		qtrue,      // keepLights
 
 		4,          // defaultSampleSize
-		{ 32, 32, 64 }, // defaultGridSize
+		{ 64, 64, 64 }, // defaultGridSize
 		qtrue,      // chamferEdges
 		1.25f,      // chamferConvexWidth
 		0.0f,       // chamferConcaveWidth
@@ -136,7 +136,7 @@ game_t gameTemplates[MAX_GAMES] = {
 		qfalse,     // keepLights
 
 		8,          // defaultSampleSize
-		{ 64, 64, 128 }, // defaultGridSize
+		{ 64, 64, 64 }, // defaultGridSize
 		qtrue,      // chamferEdges
 		1.25f,      // chamferConvexWidth
 		0.0f,       // chamferConcaveWidth
